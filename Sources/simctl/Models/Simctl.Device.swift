706d1f79eeb82218e73a48894bc7eb4068f7b961
Alexander Belyavskiy <diejmon@gmail.com>
2020-04-01 18:48:09 +0300

    Fix some devices might not have deviceTypeIdentifier for some reason

--- Sources/simctl/Models/Simctl.Device.swift
+++ Sources/simctl/Models/Simctl.Device.swift
@@ -3,7 +3,7 @@ import Foundation
 public extension Simctl {
   struct Device: Codable {
     public let name: String
-    public let deviceTypeIdentifier: SimctlIdentifier<Devicetype>
+    public let deviceTypeIdentifier: SimctlIdentifier<Devicetype>?
 
     @FilePath
     public var dataPath: URL
